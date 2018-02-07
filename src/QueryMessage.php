436eaab8bc719dc21dc2aabba5969d21ba3831ad
Vincent van Dijk <info@vincentvandijk.com>
2018-02-07 20:40:44 +0100

    Added QueryMessage.

---
+++ src/QueryMessage.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\Query;
+
+use ExtendsFramework\Message\Message;
+
+class QueryMessage extends Message implements QueryMessageInterface
+{
+}
---
+++ src/QueryMessageInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\Query;
+
+use ExtendsFramework\Message\MessageInterface;
+
+interface QueryMessageInterface extends MessageInterface
+{
+}
