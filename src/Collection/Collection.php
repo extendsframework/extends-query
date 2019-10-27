330fa792b51f12a4fd7470871aba6ca1ecf8bf13
Vincent van Dijk <info@vincentvandijk.com>
2019-10-27 17:42:56 +0100

    Added return type for key.

--- src/Collection/Collection.php
+++ src/Collection/Collection.php
@@ -55,7 +55,7 @@ class Collection implements CollectionInterface
     /**
      * @inheritDoc
      */
-    public function key()
+    public function key(): ?int
     {
         return key($this->results);
     }
